bad4878961b411b40fcc56442da73c7596fd5857
tarmstrong369 <114307720+tarmstrong369@users.noreply.github.com>
2024-08-13 12:15:42 -0700

    update breakstate

--- UserBreakState.py
+++ UserBreakState.py
@@ -3,11 +3,15 @@ from enum import Enum
 
 class UserBreakState(Enum):
     NoBreak = 1
-    NeedCozmo = 2
+    NeedCozmow = 2
+    NeedCozmob = 3
     # BreakRequest = 3
     # OnBreak = 4
     # BreakEnded = 5
-    SnoozeTriggered = 6
-    SnoozeTriggered2 = 7
-    SnoozeTriggered3 = 8
+    SnoozeTriggeredw1 = 4
+    SnoozeTriggeredw2 = 5
+    SnoozeTriggeredw3 = 6
+    SnoozeTriggeredb1 = 7
+    SnoozeTriggeredb2 = 8
+    SnoozeTriggeredb3 = 9
 
